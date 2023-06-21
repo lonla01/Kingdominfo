7ae27463fb5ce749f7e38202ff486c38770db824
Serge P. LONLA <splonla@gmail.com>
2023-06-21 15:55:51 -0700

    Improved the User resource definition. Also added a debugging header for error messages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   has_many :user_registrations, class_name: "Registration"
   has_many :bookings
   has_many :events, through: :bookings
-  validates :password, confirmation: true
+  validates :email, uniqueness: true
+  validates :password, confirmation: true, presence: true, length: { minimum: 8 }, unless: Proc.new { password.nil? }
   validates :password_confirmation, presence: true
 
   # Include default devise modules. Others available are:
