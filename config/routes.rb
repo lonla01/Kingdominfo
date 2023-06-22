0bbe5587a2d2f98ecb82680ee03482e3150cf95d
Serge P. LONLA <splonla@gmail.com>
2023-06-22 00:02:16 -0700

    Fixed an issue where link_to tag where not able to delete events. Achieved this by adding some routes in order to configure either get or delete requests for a given event id.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
            sessions: 'users/sessions',
       registrations: 'users/registrations'
     }
-    resources :events
+    resources :events, except: [:destroy]
     get 'home/index'
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -14,5 +14,11 @@ Rails.application.routes.draw do
 
     # When a user register to participate to an event
     get 'events/register/:id' => 'events#register', :as => :register_to_event
+
+    # We need thhis two lines for our delete with a link_to tag to work
+    # We use this type of link_to tag in views/events/show.html.erb
+    delete 'events/:id/delete' => 'events#destroy', :as => :event_cancel
+    get 'events/:id/delete' => 'events#destroy'
+
   end
 end
